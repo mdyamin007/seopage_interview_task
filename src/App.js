bed3dca19aef926ec5e716bde5ad7f6263406f8a
Md Yamin <mdyamin007@yahoo.com>
2023-03-27 21:14:55 +0600

    File Attachment Modal added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import CardList from "./components/CardList";
 
 export default function App() {
   return (
-    <div className="p-2 flex flex-none overflow-x-auto h-screen w-full overflow-y-hidden">
+    <div className="relative p-2 flex flex-none overflow-x-auto h-screen w-full overflow-y-hidden">
       <CardList title={"Incomplete"} />
       <CardList title={"To do"} />
       <CardList title={"Doing"} />
